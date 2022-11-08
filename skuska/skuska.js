07bbd883212b87707db7f3dcd0d55c79963b5575
xkostolanskym <102800574+xkostolanskym@users.noreply.github.com>
2022-11-08 23:53:29 +0100

    Add files via upload
    quantity button added

--- skuska/skuska.js
+++ skuska/skuska.js
@@ -16,3 +16,26 @@ function prev(){
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
+
+// quantity increment 
+
+const plus = document.querySelector('.plus'),
+minus = document.querySelector('.minus'),
+num = document.querySelector('.num');
+
+let a = 0;
+
+plus.addEventListener("click", ()=>{
+    a++;
+    a = (a < 10) ? "0" + a : a;
+    num.innerText = a;
+});
+
+
+minus.addEventListener("click", ()=>{
+    if(a > 0){
+        a--;
+        a = (a < 10) ? "0" + a : a;
+        num.innerText = a;
+    };
+});
